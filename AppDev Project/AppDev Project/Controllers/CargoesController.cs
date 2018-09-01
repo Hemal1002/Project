9309c1248c607e648b31737ef4b1a33c39b9c1f6
Hemal1002 <42447171+Hemal1002@users.noreply.github.com>
2018-09-01 15:21:28 +0200

    null

--- AppDev Project/AppDev Project/Controllers/CargoesController.cs
+++ AppDev Project/AppDev Project/Controllers/CargoesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//asdas
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
--- AppDev Project/AppDev Project/Controllers/TrucksController.cs
+++ AppDev Project/AppDev Project/Controllers/TrucksController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//asdasd
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
